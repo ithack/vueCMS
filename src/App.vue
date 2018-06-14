b508dce02b7bdba9e5a1585e2c1fdc4363270ec7
kevin <yangkai9@jd.com>
2018-06-14 21:52:21 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -32,19 +32,12 @@
         </draggable>
       </div>
     </aside>
-<<<<<<< HEAD
     <div id="view_main" v-if="!htmlCon.readOnly" @mouseover.capture="hoverFn($event)">
       <draggable element="main" class="view_box" :options="drogOptions" @add="onAdd" v-model="site.children" @choose.stop="onChoose">
-         <render v-for="(child,index) in site.children" :key="index" :node="child"/>
+        <render v-for="(child,index) in site.children" :key="index" :node="child"/>
       </draggable>
     </div>
     <render v-else v-for="child in site.children" id="view_main" :key="child.id" :node="child" :theme="site.config.color" />
-=======
-    <draggable v-if="!htmlCon.readOnly" element="main" :options="drogOptions" @add="onAdd" @sort.stop="onSort" @choose.stop="onChoose">
-       <render v-for="(child, inx) in site.children" @updata='updata' :key="child.id" :node="child" :theme="site.config.color" />
-    </draggable>
-    <render v-else v-for="child in site.children" :key="child.id" :node="child" :theme="site.config.color" />
->>>>>>> c03a73d1c12f48518ba9bc100992843a4d63f611
     <div id="config_right" v-if="!htmlCon.readOnly">
       <Config></Config>
     </div>
@@ -53,195 +46,186 @@
 
 <script>
 
-import './assets/less/rest.less'
-import './assets/less/header.less'
-import { mapState, mapActions , mapMutations} from 'vuex'
-import Draggable from 'vuedraggable'
-import $ from 'jquery'
-import Render from './common/Render'
-import Config from './common/configRight'
-import tabTree from './widgets/widgetTree'
-export default {
-  name: 'app',
-  components: {
-    Render,
-    Draggable,
-    Config
-  },
-  data () {
-    return {
-      tabIndex:0,
-      leftHide: false,
-      dragOptions: {
-        group: {
-          name: 'widgets',
-          pull: 'clone',
-          put:false,
+  import './assets/less/rest.less'
+  import './assets/less/header.less'
+  import { mapState, mapActions , mapMutations} from 'vuex'
+  import Draggable from 'vuedraggable'
+  import $ from 'jquery'
+  import Render from './common/Render'
+  import Config from './common/configRight'
+  import tabTree from './widgets/widgetTree'
+  export default {
+    name: 'app',
+    components: {
+      Render,
+      Draggable,
+      Config
+    },
+    data () {
+      return {
+        tabIndex:0,
+        leftHide: false,
+        dragOptions: {
+          group: {
+            name: 'widgets',
+            pull: 'clone',
+            put:false,
+          },
+          sort: false
         },
-        sort: false
-      },
-      drogOptions:{
-        group: {
-          name: 'widgets',
+        drogOptions:{
+          group: {
+            name: 'widgets',
+          }
         }
       }
-    }
-  },
-  computed: {
-    ...mapState(['site', 'currentConfig', 'tabTree','widgets', 'htmlCon']),
+    },
+    computed: {
+      ...mapState(['site', 'currentConfig', 'tabTree','widgets', 'htmlCon']),
 
-  },
-  /*watch: {
-    'widgets' : {
-      handler: function (val, oldVal) {
-        this.widgets=Object.assign({},val)
+    },
+    /*watch: {
+      'widgets' : {
+        handler: function (val, oldVal) {
+          this.widgets=Object.assign({},val)
+        },
+        deep: true
       },
-      deep: true
-    },
-  },*/
-  methods: {
-    ...mapMutations(['addWidget', 'sortWidget', 'setConfig','setWidget', 'setPageConfig']),
-    ...mapActions(['getSite','getDefaultConfig']),
-    tabClick(e){
-      let i=$(e.target).parent().index();
-      console.log(this.tabTree[i])
-      this.tabIndex=i
-      this.setWidget(this.tabTree[i].list)
-      this.leftHide=true;
-    },
-    switchFn(){
-      this.leftHide?this.leftHide=false:this.leftHide=true;
-    },
-    onAdd ({ item, newIndex }) {
-      const widgetType = item.getAttribute('type'),
-            model_id=item.getAttribute('model_id');
-      if(widgetType=='the-section'){
-        this.getDefaultConfig({ section: this.site.children, widgetType, newIndex ,model_id})
-      }
-    },
-    onSort ({oldIndex, newIndex, from, to}) {
-      if(from === to) {
-        this.sortWidget({array: this.site.children, oldIndex, newIndex})
-        this.currentConfig.index=-1
-      }
-    },
-    onChoose({oldIndex}){
-      this.setConfig({oldIndex, currDom: this.site})
-    },
-    hoverFn(e){
-      let self_el=$(e.target),
-        name=self_el.attr('component-name');
-      if(self_el.attr('id')!=='view_main'&&self_el.hasClass('component')){
-        self_el.hover(function () {
-          self_el.addClass('component_hover')
-        },function () {
-          self_el.removeClass('component_hover')
-        })
-      }
+    },*/
+    methods: {
+      ...mapMutations(['addWidget', 'sortWidget', 'setConfig','setWidget', 'setPageConfig']),
+      ...mapActions(['getSite','getDefaultConfig']),
+      tabClick(e){
+        let i=$(e.target).parent().index();
+        console.log(this.tabTree[i])
+        this.tabIndex=i
+        this.setWidget(this.tabTree[i].list)
+        this.leftHide=true;
+      },
+      switchFn(){
+        this.leftHide?this.leftHide=false:this.leftHide=true;
+      },
+      onAdd ({ item, newIndex }) {
+        const widgetType = item.getAttribute('type'),
+          model_id=item.getAttribute('model_id');
+        if(widgetType=='the-section'){
+          this.getDefaultConfig({ section: this.site.children, widgetType, newIndex ,model_id})
+        }
+      },
+      onSort ({oldIndex, newIndex, from, to}) {
+        if(from === to) {
+          this.sortWidget({array: this.site.children, oldIndex, newIndex})
+          this.currentConfig.index=-1
+        }
+      },
+      onChoose({oldIndex}){
+        this.setConfig({oldIndex, currDom: this.site})
+      },
+      hoverFn(e){
+        let self_el=$(e.target),
+          name=self_el.attr('component-name');
+        if(self_el.attr('id')!=='view_main'&&self_el.hasClass('component')){
+          self_el.hover(function () {
+            self_el.addClass('component_hover')
+          },function () {
+            self_el.removeClass('component_hover')
+          })
+        }
 
+      }
+    },
+    mounted () {
+      this.getSite();
     }
-  },
-  mounted () {
-    this.getSite();
   }
-}
 </script>
 
 <style lang="less">
   html, body {
     height: 100%;background-color:#F8F8F8;
   }
-.root {
-  height: 100%;
-  display: flex;
-  align-items: stretch;
-  #loading{position:absolute;z-index:99999;left:200px;top:100px;display:none;}
-}
-.component_hover{
-  border:1px solid #5E9EF3;box-sizing: border-box;position: relative;
-  &:before{
-    content:attr(component-name);position:absolute;right:0;top:0;background-color:#5E9EF3;color:#fff;
-  }
-<<<<<<< HEAD
-}
-.icon-shouqi{
-  transform: rotate(180deg);
-}
-.iconfont{
-  font-size:18px;color:#666;
-}
-.icon-white{color:#fff;}
-//left
-aside {
-  background-color:#fff;
-  position:fixed;
-  top:70px;left:0;overflow:hidden;
-  max-width:280px;
-  .left_title{
-    color:#fff;
-    background:#5E9EF3;
-    display:flex;
-    justify-content: space-between;
-    padding:6px 8px;
-    &>span{
-      display: block;width: 80%;
-    }
-    &>i{
-      text-align: center;flex: 1;
-    }
-  }
-  .tab_nav{
-    width:70px;text-align:center;border-right:1px solid #F0F0F0;float:left;padding-bottom:1000px;margin-bottom:-1000px;color:#666;
-    li{
-      padding:5px 0;
-    }
-    .active{
-      background-color:#5E9EF3;color:#fff;
-      .iconfont{color:#fff }
-    }
-    .tabIcon{
-      font-size:24px;text-align:center;display:block;width:100%;padding:4px 0;
-    }
-  }
-  .list_length{
-    width:170px;float:left;display:inline-block;fonn-size:12px;color:#D3D3D3;line-height:16px;padding:6px 5px;box-sizing:border-box;
-  }
-  .leftModel{
-    width:170px;overflow-y: scroll;display:flex;flex-wrap: wrap;justify-items: baseline;
-  }
-  & .widget-card {
-    border-radius: 2px;
-    width: 85px;margin:6px 0;
+  .root {
+    height: 100%;
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    cursor: move;
-    &>i {
-      display: block;
-      font-size: 2em;
-    }
-    &>p {
-      margin-top: 5px;
-      margin-bottom: 5px;color:#666;
+    align-items: stretch;
+    #loading{position:absolute;z-index:99999;left:200px;top:100px;display:none;}
+  }
+  .component_hover{
+    border:1px solid #5E9EF3;box-sizing: border-box;position: relative;
+    &:before{
+      content:attr(component-name);position:absolute;right:0;top:0;background-color:#5E9EF3;color:#fff;
     }
   }
-}
-// main
-#view_main{
-  background:url('./assets/img/main_bg.png') no-repeat;width:364px;height:720px;margin:0 auto;background-size:contain;position:relative;top:70px;
-  .view_box{
-    background-color:#F8F8F8;width:79%;height:72%;-webkit-transform: translate(13%,17%);border:1px solid #ccc;border-radius:5px;
-    overflow-y:scroll;
+  .icon-shouqi{
+    transform: rotate(180deg);
   }
-}
-=======
-  &>main {
-    min-width:640px;
-    margin:0 auto;
-    padding: 0 20px;
+  .iconfont{
+    font-size:18px;color:#666;
   }
-}
->>>>>>> c03a73d1c12f48518ba9bc100992843a4d63f611
+  .icon-white{color:#fff;}
+  //left
+  aside {
+    background-color:#fff;
+    position:fixed;
+    top:70px;left:0;overflow:hidden;
+    max-width:280px;
+    .left_title{
+      color:#fff;
+      background:#5E9EF3;
+      display:flex;
+      justify-content: space-between;
+      padding:6px 8px;
+      &>span{
+        display: block;width: 80%;
+      }
+      &>i{
+        text-align: center;flex: 1;
+      }
+    }
+    .tab_nav{
+      width:70px;text-align:center;border-right:1px solid #F0F0F0;float:left;padding-bottom:1000px;margin-bottom:-1000px;color:#666;
+      li{
+        padding:5px 0;
+      }
+      .active{
+        background-color:#5E9EF3;color:#fff;
+        .iconfont{color:#fff }
+      }
+      .tabIcon{
+        font-size:24px;text-align:center;display:block;width:100%;padding:4px 0;
+      }
+    }
+    .list_length{
+      width:170px;float:left;display:inline-block;fonn-size:12px;color:#D3D3D3;line-height:16px;padding:6px 5px;box-sizing:border-box;
+    }
+    .leftModel{
+      width:170px;overflow-y: scroll;display:flex;flex-wrap: wrap;justify-items: baseline;
+    }
+    & .widget-card {
+      border-radius: 2px;
+      width: 85px;margin:6px 0;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      cursor: move;
+      &>i {
+        display: block;
+        font-size: 2em;
+      }
+      &>p {
+        margin-top: 5px;
+        margin-bottom: 5px;color:#666;
+      }
+    }
+  }
+  // main
+  #view_main{
+    background:url('./assets/img/main_bg.png') no-repeat;width:364px;height:720px;margin:0 auto;background-size:contain;position:relative;top:70px;
+    .view_box{
+      background-color:#F8F8F8;width:79%;height:72%;-webkit-transform: translate(13%,17%);border:1px solid #ccc;border-radius:5px;
+      overflow-y:scroll;
+    }
+  }
   #config_right{
     position:fixed;top:70px;right:0;background-color:#fff;
   }
